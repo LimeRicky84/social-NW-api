5a419f6f1ac6e6d5ea3a7f4a434d722f3f19dfab
Ricma <ric.madse@gmail.com>
2022-08-21 23:42:00 -0600

    coding main

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addAccomplice,
+    deleteAccomplice
+} = require('../../controllers/userController')
+
+router.route('/')
+.get(getUser)
+.post(createUser);
+
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+router.route('/:userId/accomplices/:accompliceId')
+.post(addAccomplice)
+.delete(deleteAccomplice)
+
+module.exports = router
